d125c90e9be129003c20fc95a24fcf2592eace62
GekidouX <47258390+theclegginator@users.noreply.github.com>
2019-04-15 10:47:27 -0400

    window location href added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,108 +1,121 @@
-// establish basic query URL 
-// *** Need to add OBJECT LITERALS to modify our query URL w/ location from the search bar, etc.
-//let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?text=coffee&location=nyc&price=2";
+$(document).ready(function(){
+    // *** Need to add OBJECT LITERALS to modify our query URL w/ location from the search bar, etc.
+    //let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?text=coffee&location=nyc&price=2";
 
-// Our API Key
-// *** Need to make this publicly hidden later
-let apiKey = "IkthJOhbnmrhVTwbTB6OGKMzeQSlDeLB9EIS35SKpaE6_N7K7Xo_JVhnh383r_cQNRFPAo9y73ifnCRqHwsuDvxtuV3tCtKC4azI9ciyF-PgiuQbKi4i6ozdnAiwXHYx";
-const categoriesList = ["American", "BBQ", "Pizza", "Indian", "Mexican", "Italian", "Chinese", "Japanese", "Korean", "Thai", "Mediterranean", "Vegetarian"];
+    // Our API Key
+    // *** Need to make this publicly hidden later
+    let apiKey = "IkthJOhbnmrhVTwbTB6OGKMzeQSlDeLB9EIS35SKpaE6_N7K7Xo_JVhnh383r_cQNRFPAo9y73ifnCRqHwsuDvxtuV3tCtKC4azI9ciyF-PgiuQbKi4i6ozdnAiwXHYx";
+    const categoriesList = ["American", "BBQ", "Pizza", "Indian", "Mexican", "Italian", "Chinese", "Japanese", "Korean", "Thai", "Mediterranean", "Vegetarian"];
 
-// ============================================================
-// FIREBASE Initialization ====================================
-// ============================================================
+    // ============================================================
+    // FIREBASE Initialization ====================================
+    // ============================================================
 
-const config = {
-    apiKey: "AIzaSyC6JCjLeAlqzN4h32NAPlQdDKVv-Ij5qLc",
-    authDomain: "codeats-43f9e.firebaseapp.com",
-    databaseURL: "https://codeats-43f9e.firebaseio.com",
-    projectId: "codeats-43f9e",
-    storageBucket: "codeats-43f9e.appspot.com",
-    messagingSenderId: "1033500801409"
-  };
+    const config = {
+        apiKey: "AIzaSyC6JCjLeAlqzN4h32NAPlQdDKVv-Ij5qLc",
+        authDomain: "codeats-43f9e.firebaseapp.com",
+        databaseURL: "https://codeats-43f9e.firebaseio.com",
+        projectId: "codeats-43f9e",
+        storageBucket: "codeats-43f9e.appspot.com",
+        messagingSenderId: "1033500801409"
+      };
 
-firebase.initializeApp(config);
-const database = firebase.database();
-const rootReference = firebase.database().ref();
+    firebase.initializeApp(config);
+    const database = firebase.database();
+    const rootReference = firebase.database().ref();
 
-// ============================================================
-// Custom Functions ===========================================
-// ============================================================
+    // ============================================================
+    // Custom Functions ===========================================
+    // ============================================================
 
-function buildCategories() {
-    for (let i = 0; i < categoriesList.length; i++) {
-        $("#list").append(`<option>${categoriesList[i]}</option>`);
-    }
-}
-
-buildCategories();
-
-function pushFavorite(dbTestObject) {
-    // *** need to update to object literal on user name. Currently hard coded for Cody.
-    database.ref("users/cody/favorites").push(dbTestObject);
-}
-
-// Code to determine what page we are on to validate functions
-let currentPage = window.location.href.split("/").pop();
-if(currentPage === "index.html")  {
-    //$('body').append($('<script ... ></script>'))
-}
-
-// modify API query after the user hits search
-// *** may need to build in some checks later to make sure to account for undefined, etc.
-// gets the name of the page we are currently on (such as "index.html")
-
-$("#eat").on("click", function(){
-
-    let currentCategory = $("#list").val();
-    currentCategory = currentCategory.toLowerCase() + "+food";
-    // *** make SMART later
-    let currentLocation = "&location=atlanta";
-    // *** make SMART later
-    // if current price is not undefined, grab from DOM. Else, insert empty string.
-    let currentPrice = "&price=2";
-    // *** make SMART later
-    let currentRadius = "&radius=5000"
-    let queryURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${currentCategory}${currentLocation}${currentPrice}${currentRadius}&sort_by=best_match`;
-    console.log(queryURL)
-    
-
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${apiKey}`
+    function buildCategories() {
+        for (let i = 0; i < categoriesList.length; i++) {
+            $("#list").append(`<option>${categoriesList[i]}</option>`);
         }
-    }).then(function(response) {
-        //let dbTestObject = {"name": response.businesses[2].name, "id": response.businesses[2].id};
-        console.log(response)
-    });
+    }
 
-    // *** make sure it only takes them to the new page if the criteria are met.
-    window.location.href = ("results.html");
+    buildCategories();
 
-})
-let currentCategory = $("#list").val();
+    function pushFavorite(dbTestObject) {
+        // *** need to update to object literal on user name. Currently hard coded for Cody.
+        database.ref("users/cody/favorites").push(dbTestObject);
+    }
 
-// ========= Testing below
+    // Code to determine what page we are on to validate functions
+    let currentPage = window.location.href.split("/").pop();
+    if(currentPage === "index.html")  {
+        //$('body').append($('<script ... ></script>'))
+    }
+
+    // modify API query after the user hits search
+    // *** may need to build in some checks later to make sure to account for undefined, etc.
+    // gets the name of the page we are currently on (such as "index.html")
+
+    $("#eat").on("click", function(){
+        // rough code for making a modal appear if minimum search criteria is not met.
+        // $("#myBtn").click(function(){
+        //     $("#myModal").modal();
+        //   });
+
+        let currentCategory = $("#list").val();
+        currentCategory = currentCategory.toLowerCase() + "+food";
+        // *** make SMART later
+        let currentLocation = "&location=atlanta";
+        // *** make SMART later
+        // if current price is not undefined, grab from DOM. Else, insert empty string.
+        let currentPrice = "&price=2";
+        //let currentPrice = $("#price").val();
+        if (currentPrice === undefined) {
+            currentPrice = ""; // insert a blank string so as to not modify the API call.
+        }
+        // *** make SMART later
+        //let currentRadius = $("#radius").val();
+        let currentRadius = "&radius=5000"
+        if (currentRadius === undefined) {
+            currentRadius = "";
+        }
+        let queryURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${currentCategory}${currentLocation}${currentPrice}${currentRadius}&sort_by=best_match`;
+        console.log(queryURL)
+        
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${apiKey}`
+            }
+        }).then(function(response) {
+            //let dbTestObject = {"name": response.businesses[2].name, "id": response.businesses[2].id};
+            console.log(response)
+        });
+
+        // *** make sure it only takes them to the new page if the criteria are met.
+        window.location.href = ("results.html");
+
+    })
+    let currentCategory = $("#list").val();
+
+    // ========= Testing below
 
 
-// *** pulling data one time most for testing. Later, we will need to update this each time you load the lists
-rootReference.once("value") // pulls data one time 
-    .then(function(snapshot) {
-        // returns the array of objects in the users/name/favorites section
-        let favoritesList = snapshot.child("users/cody/favorites").val();
-        let blackList = snapshot.child("users/cody/blacklist").val();
+    // *** pulling data one time most for testing. Later, we will need to update this each time you load the lists
+    rootReference.once("value") // pulls data one time 
+        .then(function(snapshot) {
+            // returns the array of objects in the users/name/favorites section
+            let favoritesList = snapshot.child("users/cody/favorites").val();
+            let blackList = snapshot.child("users/cody/blacklist").val();
 
-        // This section of code loops through each item in the favorites list and appends it to a list.
-        snapshot.child("users/cody/favorites").forEach(function(favoriteSnapshot) {
-            let listItemText = favoriteSnapshot.val().name;
-            let listItemID = favoriteSnapshot.val().id;
+            // This section of code loops through each item in the favorites list and appends it to a list.
+            snapshot.child("users/cody/favorites").forEach(function(favoriteSnapshot) {
+                let listItemText = favoriteSnapshot.val().name;
+                let listItemID = favoriteSnapshot.val().id;
 
-            let listItem = $("<li>");
-            listItem.text(listItemText);
-            listItem.attr("id", listItemID);
-            // *** uncomment once we have an ID from front end team
-            // $("#content").append(listItem);
+                let listItem = $("<li>");
+                listItem.text(listItemText);
+                listItem.attr("id", listItemID);
+                // *** uncomment once we have an ID from front end team
+                // $("#content").append(listItem);
 
-        })
-    });
+            })
+        });
+});
